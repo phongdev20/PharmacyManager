6488924e6c3722f280a29f81eca987da674e7bf3
SRin285 <75055071+SRin285@users.noreply.github.com>
2022-06-01 16:39:25 +0700

    1/6

--- page/history/detailed_history.php
+++ page/history/detailed_history.php
@@ -14,45 +14,6 @@
 		</div>
 
 		<div class="main_right">
-<<<<<<< Updated upstream
-    <div style="min-height: 100vh;">
-			<div >
-				<?php include '../../component/Home/header.php' ?>
-			</div>
-      <div class="form1">
-      <div class="titles">
-        <h3>lịch sử thanh toán chi tiết</h3>
-      </div>
-      <div class="backs">
-        <button> <a href="#">trở về</a></button>
-      </div>
-      <table class="table">
-					<thead>
-					<tr class="col0">
-							<th>tên khách hàng</th>
-              <th>tên thuốc</th>
-							<th >số lượng</th>
-              <th>giá cả</th>
-						</tr>
-					</thead>
-					<tbody>
-					<tr class="col1">
-							<td>13/03/2022</td>
-							<td>nguyễn văn A</td>
-							<td>vitamin A</td>
-							<td>5</td>
-							<td>500 <span>VND</span></td>
-						</tr>
-					</tbody>
-			</table>
-      
-      
-    </div>
-    </div>
-		</div>
-    
-   
-=======
       <div style="min-height: 100vh;">
         <div >
           <?php include '../../component/Home/header.php' ?>
@@ -64,7 +25,6 @@
         
         </div> 
     </div>
->>>>>>> Stashed changes
 
 	</div>
 </body>
